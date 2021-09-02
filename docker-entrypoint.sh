1d18a4a46106c86eecce3d2193dd7bc135ea1abf
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2021-09-02 13:23:51 +0200

    Add conditional hosts entry modification

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,4 +2,8 @@
 echo "Start docker-entrypoint.sh"
 echo "Environment: ${SENTRY_ENVIRONMENT:=development}"
 
+if [ -n "$ALLEGRO_HOSTS_ENTRY" ]; then
+    echo "${ALLEGRO_HOSTS_ENTRY}" >> /etc/hosts
+fi
+
 uwsgi --uid datapunt --ini /app/uwsgi.ini
