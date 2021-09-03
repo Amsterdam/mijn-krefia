67bb91ac760e900df3398ccbfa08119f09ab5bd3
Tim van Oostrom <t.van.oostrom@amsterdam.nl>
2021-09-03 10:02:28 +0200

    Update path + service creation

--- krefia/allegro_client.py
+++ krefia/allegro_client.py
@@ -1,13 +1,22 @@
 from krefia.config import ALLEGRO_SOAP_ENDPOINT, WSDL_PATH
 from zeep import Client
 from flask import g, request
+import inspect
+from pprint import pprint
+
+
+def get_user_attributes(cls):
+    boring = dir(type("dummy", (object,), {}))
+    return [item for item in inspect.getmembers(cls) if item[0] not in boring]
 
 
 def connect():
     client = Client(wsdl=WSDL_PATH)
-    service = client.create_service("krefia_allegro", ALLEGRO_SOAP_ENDPOINT)
-    print(service)
-    print("service:", service.LoginService.AllegroWebLoginTijdelijk())
+    service = client.create_service(
+        "{http://tempuri.org/}LoginServiceBinding", ALLEGRO_SOAP_ENDPOINT
+    )
+    pprint(get_user_attributes(service))
+    print("service:", service.AllegroWebLoginTijdelijk())
     return service
 
 
